cbb2530b6278062758078a7185ce3d9e9e5a161e
Aio <AioDev.JS@gmail.com>
2021-08-05 17:24:12 +0900

    #7 Create Room - Hook Form

--- frontend/src/pages/roomCreate.tsx
+++ frontend/src/pages/roomCreate.tsx
@@ -26,9 +26,15 @@ import {
   Slider,
   TextField,
   Typography,
+  RadioGroup,
+  Radio,
+  Divider,
+  Box,
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import RoomCreateResult from '~/components/RoomCreateResult'
+import { CREATE_ROOM_MUTATION } from '~/gql/mutation'
+import { tagsList } from '~/fakeData'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,39 +49,24 @@ const useStyles = makeStyles((theme: Theme) =>
     tag: {
       margin: theme.spacing(0.5),
     },
+    label: {
+      '& label::after': {
+        content: '"*"',
+        paddingLeft: '5px',
+        color: 'red',
+      },
+    },
   })
 )
 
-interface ITag {
-  name: string
-  icon?: string
-}
-
-const tagsList: ITag[] = [
-  { name: 'tech', icon: '/icon/tech.svg' },
-  { name: 'javascript', icon: '/icon/javascript.png' },
-  { name: 'rust', icon: '/icon/rust.png' },
-  { name: 'python', icon: '/icon/python.png' },
-  { name: 'typescript', icon: '/icon/typescript.png' },
-]
-
-export const CREATE_ROOM_MUTATION = gql`
-  mutation createRoomMutation($createRoomInput: CreateRoomInput!) {
-    createRoom(input: $createRoomInput) {
-      ok
-      error
-      roomId
-      secretLink
-    }
-  }
-`
+import { ITag } from '~/fakeData'
 
 export default function RoomCreate() {
   const [isSubmit, setIsSubmit] = useState(false)
-
+  const classes = useStyles()
   const {
-    register,
     handleSubmit,
+    getValues,
     watch,
     control,
     formState: { errors },
@@ -121,7 +112,8 @@ export default function RoomCreate() {
       },
     }
   )
-
+  console.log(createRoomMutationResult)
+  console.log(watch())
   return (
     <Layout>
       <Backdrop open={createRequestLoading}>
@@ -134,7 +126,7 @@ export default function RoomCreate() {
           <Typography variant="h3" component="h3">
             Create Room
           </Typography>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Grid
               container
               spacing={3}
@@ -143,87 +135,177 @@ export default function RoomCreate() {
               alignItems="flex-start"
             >
               <Grid item xs={12}>
-                <TextField id="standard-basic" label="방이름" />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="standard-basic" label="설명" />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      name="시크릿 방으로 설정하시겠습니까 ?"
-                      color="primary"
+                <Controller
+                  name="name"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      classes={{ root: classes.label }}
+                      id="name"
+                      label="방이름"
                     />
-                  }
-                  label="시크릿 방으로 설정하시겠습니까 ?"
+                  )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox name="검색 가능" color="primary" />}
-                  label="검색 가능"
+                <Controller
+                  name="description"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField {...field} id="description" label="설명" />
+                  )}
                 />
               </Grid>
+              <Grid item xs={12}>
+                <Divider />
+                <Controller
+                  name="isSecret"
+                  control={control}
+                  defaultValue={'false'}
+                  render={({ field }) => (
+                    <RadioGroup {...field}>
+                      <Box display="flex" alignItems="center">
+                        <div>
+                          <Radio value="false" />
+                        </div>
+                        <svg
+                          height="32"
+                          viewBox="0 0 24 24"
+                          width="32"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M3 2.75A2.75 2.75 0 015.75 0h14.5a.75.75 0 01.75.75v20.5a.75.75 0 01-.75.75h-6a.75.75 0 010-1.5h5.25v-4H6A1.5 1.5 0 004.5 18v.75c0 .716.43 1.334 1.05 1.605a.75.75 0 01-.6 1.374A3.25 3.25 0 013 18.75v-16zM19.5 1.5V15H6c-.546 0-1.059.146-1.5.401V2.75c0-.69.56-1.25 1.25-1.25H19.5z"
+                          ></path>
+                          <path d="M7 18.25a.25.25 0 01.25-.25h5a.25.25 0 01.25.25v5.01a.25.25 0 01-.397.201l-2.206-1.604a.25.25 0 00-.294 0L7.397 23.46a.25.25 0 01-.397-.2v-5.01z"></path>
+                        </svg>
+                        <div>
+                          <Typography>Public</Typography>
+                          <Typography>공개로 설정합니다.</Typography>
+                        </div>
+                      </Box>
+                      <Box display="flex" alignItems="center">
+                        <div>
+                          <Radio value="true" />
+                        </div>
+                        <svg height="32" viewBox="0 0 24 24" width="32">
+                          <path d="M6 9V7.25C6 3.845 8.503 1 12 1s6 2.845 6 6.25V9h.5a2.5 2.5 0 012.5 2.5v8a2.5 2.5 0 01-2.5 2.5h-13A2.5 2.5 0 013 19.5v-8A2.5 2.5 0 015.5 9H6zm1.5-1.75C7.5 4.58 9.422 2.5 12 2.5c2.578 0 4.5 2.08 4.5 4.75V9h-9V7.25zm-3 4.25a1 1 0 011-1h13a1 1 0 011 1v8a1 1 0 01-1 1h-13a1 1 0 01-1-1v-8z"></path>
+                        </svg>
+                        <div>
+                          <Typography>Secret</Typography>
+                          <Typography>비공개로 설정합니다 .</Typography>
+                        </div>
+                      </Box>
+                    </RadioGroup>
+                  )}
+                />
+                <Box
+                  display={getValues('isSecret') === 'true' ? 'block' : 'none'}
+                >
+                  <Controller
+                    name="isCanSearched"
+                    control={control}
+                    defaultValue={true}
+                    render={({ field }) => (
+                      <FormControlLabel
+                        control={<Checkbox {...field} color="primary" />}
+                        label="검색 가능"
+                      />
+                    )}
+                  />
+                </Box>
+                <Divider />
+              </Grid>
+              <Grid item xs={12}></Grid>
               <Grid item xs={12}>
                 <Typography id="discrete-slider" gutterBottom>
-                  인원 제한한
+                  인원 제한
                 </Typography>
-                <Slider
+
+                <Controller
+                  name="maxNum"
+                  control={control}
                   defaultValue={100}
-                  aria-labelledby="discrete-slider"
-                  valueLabelDisplay="auto"
-                  step={10}
-                  marks
-                  min={10}
-                  max={100}
+                  render={({ field }) => (
+                    <Slider
+                      {...field}
+                      component="div"
+                      valueLabelDisplay="auto"
+                      step={10}
+                      marks
+                      min={10}
+                      max={100}
+                      onChange={(e, v) => {
+                        field.onChange(v)
+                      }}
+                    />
+                  )}
                 />
               </Grid>
 
               <Grid item xs={12}>
-                <Autocomplete
-                  multiple
-                  id="tags"
-                  style={{ width: 500 }}
-                  options={tagsList}
-                  classes={{}}
-                  limitTags={5}
-                  getOptionLabel={(option) => option.name}
-                  freeSolo
-                  renderOption={(option) => (
-                    <React.Fragment>
-                      <Avatar
-                        alt={option.name}
-                        sizes="small"
-                        src={option.icon}
-                      />
-                      {option.name}
-                    </React.Fragment>
+                <Controller
+                  name="tagNames"
+                  control={control}
+                  render={({ field }) => (
+                    <Autocomplete
+                      multiple
+                      freeSolo
+                      fullWidth
+                      onChange={(e, v) => {
+                        const v2 = v as ITag[]
+                        field.onChange(v2.map((i) => i.name))
+                      }}
+                      style={{ width: 500 }}
+                      options={tagsList}
+                      classes={{}}
+                      limitTags={5}
+                      getOptionLabel={(option) => option.name}
+                      noOptionsText={'No saved Tag'}
+                      renderOption={(option) => {
+                        return (
+                          <React.Fragment>
+                            <Avatar
+                              alt={option.name}
+                              sizes="small"
+                              src={option.icon}
+                            />
+                            {option.name}
+                          </React.Fragment>
+                        )
+                      }}
+                      renderInput={(params) => {
+                        console.log('renderInput', params)
+                        return (
+                          <TextField
+                            // variant="outlined"
+                            label="Tag"
+                            placeholder="Input Tag"
+                            {...params}
+                          />
+                        )
+                      }}
+                      renderTags={(value, getTagProps) =>
+                        value.map((tag, index: number) => {
+                          return (
+                            <Chip
+                              key={tag.name || tag}
+                              variant="outlined"
+                              label={tag.name || tag}
+                              avatar={
+                                tag.icon && (
+                                  <Avatar alt={tag.name} src={tag.icon} />
+                                )
+                              }
+                              {...getTagProps({ index })}
+                            />
+                          )
+                        })
+                      }
+                    />
                   )}
-                  renderTags={(value, getTagProps) =>
-                    value.map((tag, index: number) => {
-                      console.log(getTagProps({ index }))
-                      return (
-                        <Chip
-                          key={tag.name}
-                          variant="outlined"
-                          label={tag.name}
-                          avatar={<Avatar alt={tag.name} src={tag.icon} />}
-                          {...getTagProps({ index })}
-                        />
-                      )
-                    })
-                  }
-                  renderInput={(params) => {
-                    return (
-                      <TextField
-                        {...params}
-                        variant="outlined"
-                        label="Tag"
-                        placeholder="Input Tag"
-                      />
-                    )
-                  }}
                 />
               </Grid>
               <Grid item xs={12}>
